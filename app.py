0cfa71688d6c8f4dcf061738727e06bb8099fdf3
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-03-22 15:14:43 +0100

    Changed approach

--- app.py
+++ app.py
@@ -1,80 +1,67 @@
 # Welcome to Foondamate AI 
 # This will help you to solve your mathematical equations and return all the steps needed to solve the equation
+import re
 
-print('Welcome')
-
-
-def solve_linear_equation(equation):
-    try:
-        # An equation with the format ax + b = c
-        # Split the equation into left and right hand sides
-        left, right = equation.split('=')
-
-        # Split the left side into its two coefficients
-        a, b = left.split('x')
-        if a == 'x':
-            a = float(1)
-        a = float(a.strip())
-        b = float(b.replace(' ', '').strip())
-
-        # Convert the right side to a float
-        c = float(right.strip())
-
-        # check if the equation contains a plus sign
-        if '+' in left:
-            # Subtract b from both sides of the equation
-            print(f"Subtract {b} from both sides \n")
-
-            print(f"{a:.2f}x + {b:.2f} = {c:.2f} \n")
-
-            print(f"{a:.2f}x + {b:.2f} - {b:.2f} = {c:.2f} - {b:.2f} \n")
-
-            # Simplify
-            print("Simplify \n")
-            sub_ans = f"{a:.2f}x = {c-b:.2f}\n"
-            print(sub_ans)
-
-            # Divide both sides by a to isolate x
-            # print("Divide both side by common factor")
-            if a == 0:
-                raise ValueError('Invalid equation: coefficient of x cannot be zero.')
-            
-            print(f"Divide both sides by {a:.2f} \n")
-
-            print(f"{a}x = {c-b:.2f} \n")
-            x = (c - b) / a
-            print(f"x = {x:.2f} \n")
-
-        # check if the equation contains a minus sign
-        elif '-' in left:
-            # Add b to both sides of the equation
-            print(f"Add {b} to both sides \n")
-
-            print(f"{a:.2f}x - {b:.2f} = {c:.2f} \n")
-
-            print(f"{a:.2f}x - {b:.2f} + {b:.2f} = {c:.2f} + {b:.2f} \n")
-
-            # Simplify
-            print("Simplify \n")
-            add_ans = f"{a:.2f}x = {c+b:.2f}\n"
-            print(add_ans)
-
-            # Divide both sides by a to isolate x
-
-            if a == 0:
-                raise ValueError('Invalid equation: coefficient of x cannot be zero.')
-            
-            print(f"Divide both sides by {a:.2f} \n")
-
-            print(f"{a}x = {c+b:.2f} \n")
-            x = (c + b) / a
-            print(f"x = {x:.2f} \n")
-
-        # Return the solution 
-        return f"x = {x:.2f}"
-    except (ValueError, TypeError) as e:
-        # Handle errors by printing an error message
-        print(f"Error: {e}")
-        return None, None
+def solve_linear_equation():
+    """
+    Solves a linear equation in one variable and returns the steps taken to solve the equation.
+    The equation should be entered in the format "ax + b = c", where a, b, and c are integers or decimals.
+    """
+    # Get the equation from the user
+    equation = input("Enter a linear equation in one variable (e.g. 'ax - b = c'): ")
     
+    # Parse the equation using regular expressions
+    match1 = re.match(
+        r'^\s*(?P<a>[-+]?(\d+(\.\d*)?|\.\d+))\s*x\s*[-+]\s*'
+        r'(?P<b>[-+]?(\d+(\.\d*)?|\.\d+))\s*=\s*'
+        r'(?P<c>[-+]?(\d+(\.\d*)?|\.\d+))\s*$',
+        equation
+    )
+    
+    if not match1:
+        print("Invalid equation format. Please enter the equation in the format 'ax + b = c', where a, b, and c are integers or decimals.")
+        return
+    
+    # Extract the coefficients and constants from the equation
+    a = float(match1.group('a'))
+    b = float(match1.group('b'))
+    c = float(match1.group('c'))
+    
+    # Solve the equation
+    steps = []
 
+    # check if the equation contains a plus sign or a minus sign
+    if '+' in equation:
+        # Subtract b from both sides of the equation
+        steps.append(f"Original equation: {equation} \n")
+        steps.append(f"Subtract {b:.2f} from both sides:\n")
+        steps.append(f"{a:.2f}x + {b:.2f} - {b:.2f} = {c:.2f} - {b:.2f}\n")
+        steps.append("Simplify the equation \n")        
+        steps.append(f"{a:.2f}x = {c-b:.2f}\n")
+
+        # Divide both sides by a to isolate x
+        steps.append("Dividing both sides by {}:".format(a) + "\n")
+        x = (c-b)/a
+        steps.append("x = {:.2f}".format(x))
+    elif '-' in equation:
+        # Add b to both sides of the equation
+        steps.append(f"Original equation: {equation} \n")
+        steps.append(f"Add {b:.2f} to both sides:\n")
+        steps.append(f"{a:.2f}x - {b:.2f} + {b:.2f} = {c:.2f} + {b:.2f}\n")
+        steps.append("Simplify the equation \n")
+        steps.append(f"{a:.2f}x = {c+b:.2f}\n")
+
+        # Divide both sides by a to isolate x
+        steps.append("Dividing both sides by {}:".format(a) + "\n")
+        x = (c+b)/a
+        steps.append("x = {:.2f}".format(x))
+        
+    
+    # Print the steps
+    for step in steps:
+        print(step)
+    
+    return x
+    
+    
+    
--- run_app.py
+++ run_app.py
@@ -2,14 +2,6 @@ from app import solve_linear_equation
 
 
 if __name__ == '__main__':
-    # Prompt the user for the equation
-    equation = input("Enter an equation in the form ax + b = c: ")
 
-    # Solve the equation
-    x = solve_linear_equation(equation)
+    solve_linear_equation()
 
-    # Print the solution
-    if x is not None:
-        print("Solved")
-    else:
-        print("Not solved")
